49acb4e350c2b51550918eaeb6ef813b193930bb
justmee134 <86782234+justmee134@users.noreply.github.com>
2021-07-01 19:54:04 +0700

    Update config.js

--- config.js
+++ config.js
@@ -5,8 +5,8 @@ const CONFIG = {
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi :)))',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    title: 'hú le yangho',
+    desc: 'm bị khùng phải k ??? ',
     btnYes: 'T thề là t bị khùng <33',
     btnNo: 'Không nha Khôi khùng :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao có mình m khùng z <3',
@@ -15,5 +15,5 @@ const CONFIG = {
     mess: 't biết mà 🥰. t bt m rất là khùng 😘😘',
     messDesc: 'sáng 7/7 có hẹn trước cổng trường quánh nhao nhé.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.facebook.com/khoi.honguyen.376/' 
+    messLink: 'https://www.facebook.com/messages/t/100007037674897' 
 }
