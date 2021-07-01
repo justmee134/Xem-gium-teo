b2ae8268cc89aec9baabb1eb2d01f4dda3ac66d6
justmee134 <86782234+justmee134@users.noreply.github.com>
2021-07-01 20:02:11 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'chị đại à!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
+    title: 'Có phải m bị khùng hong ?',
+    desc: 'T đoán là có',
+    btnYes: 'T bị khùng thiệt nha',
+    btnNo: 'Không nhá mịa m',
+    question:'Trên thế giới hơn 7 tỉ người mà sao có mình m bị khùng z <3',
+    btnReply: 'Gửi lại choa t <3',
+    reply: 'khùng thì khùng mà không khùng thì khùng',
+    mess: 'T biết mà, cuối cùng cũng đã chịu nhận',
+    messDesc: 'Hẹn 7/7 lên trường quánh nhao nhó.',
+    btnAccept: 'Okiiiii lun mịa m',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
