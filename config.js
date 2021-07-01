6085645143bae44549845c820738471916c5bea4
justmee134 <86782234+justmee134@users.noreply.github.com>
2021-07-01 20:50:55 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Chị đại!',
+    introTitle: 'Du à ',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'j đếy :)))',
-    title: 'hú le chị đại',
-    desc: 'm bị khùng phải k ??? ',
-    btnYes: 'T thề là t bị khùng <33',
-    btnNo: 'Không nha Khôi khùng :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao có mình m khùng z <3',
+    title: 'hú le lợn',
+    desc: 'm có thấy t bị điên k ??? ',
+    btnYes: 'không cóa nha<33',
+    btnNo: 'm khùng trước h mà hỏi t chi :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao có mình t bth z ta <3',
     btnReply: 'Gửi cho khôi neh <3',
-    reply: 'khùng thì khùng mà không khùng thì khùng <33333333',
-    mess: 't biết mà. t bt m rất là khùng',
-    messDesc: 'sáng 7/7 có hẹn trước cổng trường quánh nhao nhé.',
+    reply: 'iu ghê heheh<33333333',
+    mess: 't biết mà. t bt m sẽ bảo t bth mà',
+    messDesc: 'sáng 7/7 thi tốt nhó.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/messages/t/100007037674897' 
 }
