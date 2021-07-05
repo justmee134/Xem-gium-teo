91f0b61c166104b771fc921810ea76d3d2a0fbe6
just me <86782234+justmee134@users.noreply.github.com>
2021-07-05 10:51:15 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Du à ',
+    introTitle: 'yangho à ',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'j đếy :)))',
-    title: 'hú le lợn',
+    btnIntro: 'j đấy thằng kia :)))',
+    title: 'hú le yangho',
     desc: 'm có thấy t bị điên k ??? ',
     btnYes: 'không cóa nha<33',
     btnNo: 'm khùng trước h mà hỏi t chi :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao có mình t bth z ta <3',
-    btnReply: 'Gửi cho khôi neh <3',
+    btnReply: 'ta gửi cho ngươi này <3',
     reply: 'iu ghê heheh<33333333',
     mess: 't biết mà. t bt m sẽ bảo t bth mà',
-    messDesc: 'sáng 7/7 thi tốt nhó.',
+    messDesc: 'sáng 7/7 có hẹn trước cổng trường nhó.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/messages/t/100007037674897' 
 }
